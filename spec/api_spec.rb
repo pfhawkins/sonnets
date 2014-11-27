9da3ba13f72c482c73e9b47c6f7ef8b965ed3edb
P.F. Hawkins <pf@pfhawkins.com>
2014-11-27 07:01:39 -0500

    Dedupe rspec describe declaration.

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -8,25 +8,23 @@ describe Sonnet::API do
     Sonnet::API
   end
 
-  describe Sonnet::API do
-    describe 'GET /' do
-      it 'does not error out' do
-        get "/"
-        expect(last_response.status).to eq(200)
-        expect(last_response).to be_ok
-      end
+  describe 'GET /' do
+    it 'does not error out' do
+      get "/"
+      expect(last_response.status).to eq(200)
+      expect(last_response).to be_ok
+    end
 
-      it 'has an application/vnd.api+json content type' do
-        header 'Content-Type', 'application/vnd.api+json'
-        get "/"
-        expect(last_response.header).to include({"Content-Type" => "application/vnd.api+json"})
-      end
+    it 'has an application/vnd.api+json content type' do
+      header 'Content-Type', 'application/vnd.api+json'
+      get "/"
+      expect(last_response.header).to include({"Content-Type" => "application/vnd.api+json"})
+    end
 
-      it 'contains a poets object' do
-        get "/"
-        expect(last_response.body).to include("poets")
-        expect(last_response.body).to include_json(%("poets"))
-      end
+    it 'contains a poets object' do
+      get "/"
+      expect(last_response.body).to include("poets")
+      expect(last_response.body).to include_json(%("poets"))
     end
   end
 end
